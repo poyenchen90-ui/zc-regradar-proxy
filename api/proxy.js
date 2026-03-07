1313154c1043dfbeb5807e307c85cb00b3156949
poyenchen90-ui <poyen.chen90@gmail.com>
2026-03-07 18:14:00 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,3 +1,4 @@
+// v2 - supports Anthropic + Airtable
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
